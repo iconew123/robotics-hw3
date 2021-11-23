1908a919e4f6c22f615ac286e363a223bea6e86c
iconew123 <94097685+iconew123@users.noreply.github.com>
2021-11-24 03:41:35 +0900

    Update Vector_subscriber.py

--- algorithm/src/Vector_subscriber.py
+++ algorithm/src/Vector_subscriber.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import rospy
-from Vector3.msg import Vector3
+from Twist.msg import Twist
 
 def callback(msg):
     print "subscribe:", msg.timestamp.secs%100, msg.pose.x, msg.pose.y, msg.pose.z
 
-rospy.init_node('Vector3_subscriber')
-sub = rospy.Subscriber('Vector3_msg', TimePose, callback)
+rospy.init_node('subscriber_algorithm')
+sub = rospy.Subscriber('Twist_msg', Twist, callback)
 rospy.spin()
